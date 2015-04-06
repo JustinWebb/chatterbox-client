2f1f9c2276035900a7fcdc0c49605703273d2755
JustinWebb <jw@justinwebb.io>
2015-04-06 16:12:15 -0700

    Implement basic app object with send and fetch functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,58 +1,99 @@
-// YOUR CODE HERE:
+
+var app = (function(){
+  // Private variables
 
 
+  // Public API
+  return {
+      results:[],
+
+      server: 'https://api.parse.com/1/classes/chatterbox',
+
+      init:function(){
+
+      },
+      send:function(input){
+        $.ajax({
+          // always use this url
+          url: 'https://api.parse.com/1/classes/chatterbox',
+          type: 'POST',
+          data: JSON.stringify(input),
+          contentType: 'application/json',
+          success: function (data) {
+            console.log('chatterbox: Message sent' + message);
+          },
+          error: function (data) {
+            // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+            console.error('chatterbox: Failed to send message');
+          }
+        });
+      },
+
+      fetch: function(){
+
+        $.ajax({
+          // always use this url
+          url: this.server,
+          type: 'GET',
+          data: /*{'order=-createdAt', 'limit=10'}*/
+          {
+             order:'-createdAt',
+             limit: 10
+             // count: 1
+             // include: "something"
+          },
+          contentType: 'application/json',
+          //dataType:'jsonp',
+          success: function (data) {
+            //console.error('chatterbox: Failed to send message');
+            // ourData
+            var $messages = $('#messages');
+            _.each(data.results, function(item){
+              $messages.children('li').remove();
+              $messages.append('<li class="chat">' +
+                                      '<p class="userName">' + item.username + ' ' + item.roomname + '</p>'
+                                      + '<p>' + item.text + '</p>'
+
+
+                                    +'</li>');
+            });
+          },
+          error: function (data) {
+            console.error('chatterbox: Failed to send message');
+          }
+        });
+      }
+
+    };
+})();
 // on load
-$(function(){
-	$('#submitMessage').click(function(e){
+// $(function(){
+//   $('#submitMessage').click(function(e){
 
-		e.preventDefault();
-		var message = {
-			'username': 'jyeg',
-			'text': $('#messageText').val(),
-			'roomname': 'kings_landing'
-		};
+//     e.preventDefault();
+//     var message = {
+//       'username': 'jyeg',
+//       'text': $('#messageText').val(),
+//       'roomname': 'kings_landing'
+//     };
 
-		$.ajax({
-			// always use this url
-			url: 'https://api.parse.com/1/classes/chatterbox',
-			type: 'POST',
-			data: JSON.stringify(message),
-			contentType: 'application/json',
-			success: function (data) {
-				console.log('chatterbox: Message sent' + message);
-			},
-			error: function (data) {
-				// see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-				console.error('chatterbox: Failed to send message');
-			}
-		});
-	});
-	var refresh = function(){
+//     $.ajax({
+//       // always use this url
+//       url: 'https://api.parse.com/1/classes/chatterbox',
+//       type: 'POST',
+//       data: JSON.stringify(message),
+//       contentType: 'application/json',
+//       success: function (data) {
+//         console.log('chatterbox: Message sent' + message);
+//       },
+//       error: function (data) {
+//         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//         console.error('chatterbox: Failed to send message');
+//       }
+//     });
+//   });
 
-		$.ajax({
-			// always use this url
-			url: 'https://api.parse.com/1/classes/chatterbox',
-			type: 'GET',
-			data: 'order=-createdAt',
-			contentType: 'application/json',
-			//dataType:'jsonp',
-			success: function (data) {
-				console.error('chatterbox: Failed to send message');
-				_.each(data.results, function(item){
-					$('#messages').append('<li class="chat">' +
-																	'<p class="userName">' + item.username + ' ' + item.roomname + '</p>'
-																	+ '<p>' + item.text + '</p>'
-
-
-																+'</li>');
-				});
-			},
-			error: function (data) {
-				console.error('chatterbox: Failed to send message');
-			}
-		});
-	}
-	setInterval(refresh(), 200);
-});
+//   //setInterval(refresh, 1000);
+// });
 
 
