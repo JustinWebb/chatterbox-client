db255769ce7f4168329aad633b3ff66605377eb3
John <JmYeglinski@gmail.com>
2015-04-06 14:47:27 -0700

    Initial requirements to set up basic app. Makes ajax request to parse server and can submit a message.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,58 @@
 // YOUR CODE HERE:
 
+
+// on load
+$(function(){
+	$('#submitMessage').click(function(e){
+
+		e.preventDefault();
+		var message = {
+			'username': 'jyeg',
+			'text': $('#messageText').val(),
+			'roomname': 'kings_landing'
+		};
+
+		$.ajax({
+			// always use this url
+			url: 'https://api.parse.com/1/classes/chatterbox',
+			type: 'POST',
+			data: JSON.stringify(message),
+			contentType: 'application/json',
+			success: function (data) {
+				console.log('chatterbox: Message sent' + message);
+			},
+			error: function (data) {
+				// see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+				console.error('chatterbox: Failed to send message');
+			}
+		});
+	});
+	var refresh = function(){
+
+		$.ajax({
+			// always use this url
+			url: 'https://api.parse.com/1/classes/chatterbox',
+			type: 'GET',
+			data: 'order=-createdAt',
+			contentType: 'application/json',
+			//dataType:'jsonp',
+			success: function (data) {
+				console.error('chatterbox: Failed to send message');
+				_.each(data.results, function(item){
+					$('#messages').append('<li class="chat">' +
+																	'<p class="userName">' + item.username + ' ' + item.roomname + '</p>'
+																	+ '<p>' + item.text + '</p>'
+
+
+																+'</li>');
+				});
+			},
+			error: function (data) {
+				console.error('chatterbox: Failed to send message');
+			}
+		});
+	}
+	setInterval(refresh(), 200);
+});
+
+
